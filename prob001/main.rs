fc38cb0060f95ec6b6a04e7edc032470bd816900
Stacey Ell <stacey.ell@gmail.com>
2014-08-20 17:07:52 -0600

    prob001: use spaces for indentation

--- prob001/main.rs
+++ prob001/main.rs
@@ -2,6 +2,6 @@ use std::iter::AdditiveIterator;
 
 
 fn main() {
-	let sum = range(0_u32, 1000).filter(|&x| x % 3 == 0 || x % 5 == 0).sum();
-	println!("sum = {}", sum);
+    let sum = range(0_u32, 1000).filter(|&x| x % 3 == 0 || x % 5 == 0).sum();
+    println!("sum = {}", sum);
 }
