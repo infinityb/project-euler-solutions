36fe7ea1060640a77fb7fff4e33d8798a3a141c9
Stacey Ell <stacey.ell@gmail.com>
2014-08-20 14:40:47 -0600

    Initial commit

---
+++ prob10/main.rs
@@ -0,0 +1,40 @@
+use std::iter::AdditiveIterator;
+
+
+struct PrimeGen {
+    primes: Vec<u64>,
+    n: u64
+}
+
+
+impl PrimeGen {
+    fn new() -> PrimeGen {
+        PrimeGen {
+            primes: Vec::with_capacity(2000000),
+            n: 2
+        }
+    }
+}
+
+impl Iterator<u64> for PrimeGen {
+    fn next(&mut self) -> Option<u64> {
+        loop {
+            let mut is_prime = true;
+            for prime in self.primes.iter() {
+                if self.n % *prime == 0 {
+                    is_prime = false;
+                    break;
+                }
+            }
+            if is_prime {
+                self.primes.push(self.n);
+                return Some(self.n);
+            }
+            self.n += 1;
+        }
+    }
+}
+
+fn main() {
+    println!("n = {}", PrimeGen::new().take_while(|&f| f < 2000000_u64).sum());
+}
---
+++ prob14/main.rs
@@ -0,0 +1,71 @@
+use std::sync::deque::{BufferPool, Data, Empty, Abort};
+
+type CollatzInt = u64;
+
+#[inline]
+fn collatz_next(num: CollatzInt) -> CollatzInt {
+    match num % 2 == 0 {
+        true => num / 2,
+        false => 3 * num + 1
+    }
+}
+
+#[inline]
+fn collatz_length(num: CollatzInt) -> u32 {
+    let mut value = num;
+    let mut iteration = 1_u32;
+    while value != 1 {
+        value = collatz_next(value);
+        iteration += 1;
+    }
+    iteration
+}
+
+fn main() {
+    let (tx, rx) = channel();
+    let (worker, stealer) = BufferPool::new().deque();
+
+    for i in range(1 as CollatzInt, 1000000) {
+        worker.push(i);
+    }
+
+    let jobs = std::os::num_cpus();
+
+    for _ in range(0, jobs) {
+        let child_stealer = stealer.clone();
+	let finish_tx = tx.clone();
+
+        spawn(proc() {
+            let mut longest: (u32, CollatzInt) = (1, 1);
+            loop {
+                match child_stealer.steal() {
+                    Data(num) => {
+                        let (len, _) = longest;
+                        let new_len = collatz_length(num);
+                        if new_len > len {
+                            longest = (new_len, num);
+                        }
+                    },
+                    Empty => break,
+                    Abort => ()
+                }
+            }
+            finish_tx.send(longest);
+        });
+    }
+
+    let mut results = Vec::new();
+    for _ in range(0, jobs) {
+	results.push(rx.recv());
+    }
+
+    let mut longest: (CollatzInt, u32) = (1, 1);
+    for &(new_len, num) in results.iter() {
+        let (_, len) = longest;
+        if new_len > len {
+            longest = (num, new_len);
+        }
+    }
+    let (len, num) = longest;
+    println!("{} with length of {}", len, num);
+}
