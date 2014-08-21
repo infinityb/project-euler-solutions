6bc5fbcb57b6357540b719e35585da39a8b01b78
Stacey Ell <stacey.ell@gmail.com>
2014-08-20 18:37:28 -0600

    Solution 006

---
+++ prob006/main.rs
@@ -0,0 +1,16 @@
+use std::iter::AdditiveIterator;
+use std::num::pow;
+
+fn main() {
+    let sum_of_sqrs = range(0_u64, 101).map(|x| pow(x, 2)).sum();
+    let sqr_of_sums = pow(range(0_u64, 101).sum(), 2);
+    println!("result = {}", sqr_of_sums - sum_of_sqrs);
+}
+
+
+#[test]
+fn test_euler_result() {
+    let sum_of_sqrs = range(0_u64, 11).map(|x| pow(x, 2)).sum();
+    let sqr_of_sums = pow(range(0_u64, 11).sum(), 2);
+    assert_eq!(sqr_of_sums - sum_of_sqrs, 2640);
+}
